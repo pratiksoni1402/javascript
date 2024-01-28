5d8e6214a3cd4bf1cdfa018447394e7231667d7d
Pratik <pratiksoni1402@gmail.com>
2024-01-28 22:38:21 +0530

    string and template literals revised

--- jscript/string-method.js
+++ jscript/string-method.js
@@ -1,3 +1,6 @@
-const finalmessage = "Pratik"
-const message = finalmessage.includes('TI')
-console.log(message) 
+const lastdigit = '2345'
+const result =  lastdigit.padStart(16, '*')
+console.log(result)
+const resulttwo  = lastdigit.padEnd(16, '*')
+console.log(resulttwo)
+
