d031ee4dd36397de2821994bbfe14beaf58f10b9
Pratik <pratiksoni1402@gmail.com>
2024-04-14 11:20:30 +0530

    deep copy and shallow copy done

---
+++ learning/deep-shallow-copy.js
@@ -0,0 +1,9 @@
+// const fruits = ['pratik', 'rakesh', 'jalani', 'prakash']
+// const cloning = [...fruits]
+// console.log("Cloned fruits", cloning)
+// cloning.push("Hello")
+// console.log("Adding", cloning)
+// console.log("Old array", fruits)
+
+const arr = []
+console.log(typeof arr)
