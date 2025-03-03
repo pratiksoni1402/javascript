5b5fe1361c03b1675c63891acc41a380a4f78d62
pratik <pratiksoni1402@gmail.com>
2025-03-03 16:09:26 +0530

    learning promises

---
+++ Practice/Practice Promises, Async Await/Promises/promise.js
@@ -0,0 +1,11 @@
+// let promise = new Promise(function (resolve, reject){
+//   // executor (the producing code, "singer")
+// })
+
+let newPromise = new Promise((resolve, reject)=>{
+  setTimeout(()=> resolve('done'), 1000)
+})
+  newPromise.then(
+    result=> console.log(result),
+    error=> console.log(error)
+  );
