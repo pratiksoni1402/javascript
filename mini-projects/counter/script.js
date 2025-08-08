f67aa5c759b890d40d55c318be23415f306502bc
Pratik <pratiksoni1402@gmail.com>
2025-08-08 22:43:33 +0530

    adding min max value constraint to counter

--- mini-projects/counter/script.js
+++ mini-projects/counter/script.js
@@ -4,27 +4,47 @@ const displayCount = document.getElementById('count');
 const increaseCount = document.getElementById('increment');
 const decreaseCount = document.getElementById('decrement');
 const resetCount = document.getElementById('reset');
-const minValue = document.getElementById('min-value');
-const maxValue = document.getElementById('max-value');
+const getMinValue = document.getElementById('min-value');
+const getMaxValue = document.getElementById('max-value');
 
+let maxValue;
+let minValue;
+console.log(maxValue)
 function updateUi() {
   displayCount.textContent = count;
  decreaseCount.disabled = count ===0
   resetCount.disabled = count ===0
 }
 
+getMinValue.addEventListener('change', (e)=>{
+  displayCount.textContent = e.target.value
+})
+
+getMaxValue.addEventListener('change', (e)=>{
+  maxValue = e.target.value
+
+})
+
 // Initialize on load
 updateUi();
 
 increaseCount.addEventListener('click', () => {
-  count++;
-  updateUi();
+    console.log('Max value', maxValue)
+  if(displayCount.textContent < +maxValue){
+    displayCount.textContent+=1;
+    updateUi();
+  }else{
+    console.log('Cannot perform increment beyond maximum value')
+  }
 });
 
 decreaseCount.addEventListener('click', () => {
-  if (count > 0) {
+  if (displayCount.textContent > +minValue) {
     count--;
     updateUi();
+  }else{
+    console.log('Cannot perform decrement beyond minimum value')
+
   }
 });
 
